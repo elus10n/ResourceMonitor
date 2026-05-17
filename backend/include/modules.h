e098ceb1f15d7ff695356ea5b081c42c2d632679
elus10n <dizbvalkyre@gmail.com>
2026-05-17 17:50:00 +0300

    parser design

--- backend/include/modules.h
+++ backend/include/modules.h
@@ -1,4 +1,60 @@
 #ifndef MODULES_H
 #define MODULES_H
 
+///Parser
+struct ParserOutput
+{
+    CPUData cpu_data;
+    MemoryData memory_data;
+    std::vector<ProcData> proc_data;
+};
+
+struct CPUData
+{
+    std::vector<KernelData> kernel_data;
+};
+
+struct KernelData
+{
+    std::string name;
+
+    //jiffies
+    uint64_t user_j;
+    uint64_t nice_j;
+    uint64_t system_j;
+    uint64_t idle_j;
+    uint64_t iowait_j;
+    uint64_t irq_j;
+    uint64_t softirq_j;
+    uint64_t steal_j;
+    uint64_t guest_j;
+    uint64_t guest_nice_j;
+};
+
+struct MemoryData
+{
+    //kb
+    uint64_t mem_total;
+    uint64_t mem_free;
+    uint64_t mem_available;
+};
+
+struct ProcData
+{
+    // /proc/[pid]/stat
+    pid_t pid;
+    std::string e_name;
+    char state;
+    uint64_t user_j;
+    uint64_t system_j;
+
+    // /proc/[pid]/statm
+    uint64_t size; //pages
+    uint64_t resident; //pages
+
+    // /proc/[pid]/cmdline
+    std::string command;
+};
+///Parser
+
 #endif
--- backend/include/parser.h
+++ backend/include/parser.h
@@ -1,4 +1,25 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <vector>
+#include <filesystem>
+#include <string>
+#include <sys/types.h>
+
+#include "modules.h"
+
+class Parser
+{
+    CPUData cpu_parse() const;
+
+    MemoryData mem_parse() const;
+
+    std::vector<ProcData> processes_parse() const;
+
+    ProcData process_parse(pid_t pid) const;
+
+    public:
+    ParserOutput parse() const;
+};
+
 #endif
